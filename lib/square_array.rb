9e673f92f851a388a7387ea0e231301ec10b10de
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-25 20:45:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,7 @@ def square_array(array)
   i = 0
   new_array = []
   while i < array.length do
-    number = (i+1) ** 2
-    i += 1
-    new_array << number
+    new_array << array[i] ** 2
   end
   new_array
 end
